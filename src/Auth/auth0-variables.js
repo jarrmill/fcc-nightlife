4abcc4001cc7026e698309134e62f6c298547232
Jarrod <jarrod.miller@ttu.edu>
2017-11-30 14:04:43 -0700

    changed Auth0 settings

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,6 +1,7 @@
 export const AUTH_CONFIG = {
   domain: 'jmillie.auth0.com',
-  clientId: 'm2WDuPtIQOdSA9KbTvGjeu4b4fQ2vEg2',
-  callbackUrl: 'http://localhost:3000/callback',
+  //clientId: 'm2WDuPtIQOdSA9KbTvGjeu4b4fQ2vEg2',
+  clientId: 'x1wIj4fxvtsihep5nYVR4wlYi4ydpuXP',
+  callbackUrl: 'https://jsnightlifefcc.herokuapp.com/callback',
   apiUrl: 'https://jmillie.auth0.com/api/v2/'
 }
